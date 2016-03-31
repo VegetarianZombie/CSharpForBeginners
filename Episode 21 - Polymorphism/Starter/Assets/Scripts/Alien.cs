a7182654f99354a1846c4ad6f56cf2e7d70af03f
Brian Moakley <brian.douglas.moakley@gmail.com>
2016-03-31 08:44:17 -0400

    Sets up project for recording

---
+++ Episode 21 - Polymorphism/Starter/Assets/Scripts/Alien.cs
@@ -0,0 +1,21 @@
+﻿namespace Jezner.CSharp {
+
+	public struct Alien { 
+
+		public int PointValue { get;set; }
+		public int HitPoints { get;set; }
+		public bool IsAlive { get;set; }
+
+		public Alien(int pointValue, int hitPoints, bool isAlive) : this() {
+			this.PointValue = pointValue;
+			this.HitPoints = hitPoints;
+			this.IsAlive = isAlive;
+		}
+
+		public Alien(int pointValue) : this(pointValue, 100, true) {
+
+		}
+
+	}
+
+}
