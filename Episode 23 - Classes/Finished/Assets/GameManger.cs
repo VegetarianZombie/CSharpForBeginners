d26c7d0528ae243d10c5a72c754f74d0eb4fcee1
Brian Moakley <brian.douglas.moakley@gmail.com>
2016-04-17 09:29:35 -0400

    Adds completed final project for classes

--- Episode 23 - Classes/Finished/Assets/GameManger.cs
+++ Episode 23 - Classes/Finished/Assets/GameManger.cs
@@ -5,23 +5,27 @@ using Beginning.CSharp;
 
 public class GameManger : MonoBehaviour {
 
-	private IShootable[] enemies = new IShootable[4];
-	private IPersistable[] savedItems = new IPersistable[3];
-	
-	void Start () {
-		Alien alienOne = new Alien();
-		Turret turret = new Turret();
-		Player player = new Player();
-
-		savedItems[0] = alienOne;
-		savedItems[1] = turret;
-		savedItems[2] = player;
-	}
-
 	void OnDisable() {
-		foreach (IPersistable persistable in savedItems) {
-			persistable.Save();
-		}
+		Alien alien = new Alien();
+		alien.HitPoints = 100;
+
+		Alien anotherAlien = alien;
+		anotherAlien.HitPoints = 150;
+
+		Debug.Log("Alien 1 Hit Points: " + alien.HitPoints);
+		Debug.Log("Alien 2 Hit Points: " + anotherAlien.HitPoints);
+
+		Player playerOne = new Player();
+		playerOne.Name = "Brian";
+		Player playerTwo = playerOne;
+		playerTwo.Name = "Max";
+
+		Debug.Log("Player 1's Name: " + playerOne.Name);
+		Debug.Log("Player 2's Name: " + playerTwo.Name);
+
+		playerOne = null;
+		Debug.Log("Player 2's Name: " + playerTwo.Name);
+
 	}
 
 
